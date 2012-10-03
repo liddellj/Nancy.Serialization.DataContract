4b94fc6438464f831acef20cb3fe7ae8859ed1d2
Jim Liddell <jim.liddell@linn.co.uk>
2012-10-03 10:23:52 +0100

    Initial commit.

---
+++ src/Nancy.Serialization.DataContract/Nancy.Serialization.DataContract.Tests/Fakes/DataContractModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Nancy.Serialization.DataContract.Tests.Fakes
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class DataContractModel
+    {
+        [DataMember(Name = "anInt")]
+        public int AnInt { get; set; }
+    }
+}
---
+++ src/Nancy.Serialization.DataContract/Nancy.Serialization.DataContract.Tests/JsonDataContractSerializerFixture.cs
@@ -0,0 +1,33 @@
+﻿namespace Nancy.Serialization.DataContract.Tests
+{
+    using System.IO;
+    using System.Text;
+
+    using Nancy.Serialization.DataContract.Tests.Fakes;
+
+    using Xunit;
+
+    public class JsonDataContractSerializerFixture
+    {
+        private readonly JsonDataContractSerializer serializer;
+
+        public JsonDataContractSerializerFixture()
+        {
+            this.serializer = new JsonDataContractSerializer();
+        }
+
+        [Fact]
+        public void Should_recognise_data_member_attributes()
+        {
+            // Given
+            var model = new DataContractModel { AnInt = 45 };
+            var stream = new MemoryStream();
+
+            // When
+            this.serializer.Serialize("application/json", model, stream);
+
+            // Then
+            Assert.Equal("{\"anInt\":45}", Encoding.ASCII.GetString(stream.ToArray()));
+        }
+    }
+}
